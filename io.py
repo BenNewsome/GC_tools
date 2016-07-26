e792b82a03befa462868de0ff47d718e95da6547
Ben Newsome <bn506@york.ac.uk>
2016-07-26 03:25:58 +0100

    Updated to use PyGChem netCDF save instead of iris

--- io.py
+++ io.py
@@ -21,6 +21,10 @@ def open_netCDF(folder='none',filename='ctm.nc'):
     import os
     if (folder == 'none'):
         folder = os.getcwd()
+    # Strip trailing "/"
+    elif (folder[-1]=="/"):
+        folder = folder[:-1]
+        
     netCDF_filename = os.path.join(folder, filename)
            
 
@@ -40,21 +44,40 @@ def open_netCDF(folder='none',filename='ctm.nc'):
         except:
             import pygchem.datafields as datasets
         import glob
-        bpch_files = glob.glob(folder + '/*.bpch*' )
+        bpch_files = glob.glob(folder + '/*.bpch' )
         logging.debug('Found ' + str(len(bpch_files)) + ' bpch files.')
         if (len(bpch_files) == 0):
             logging.error('No bpch files found.')
-            raise IOError('Cannot find bpch files.')
+            raise IOError('Cannot find bpch files in {folder}'\
+                        .format(folder=folder))
             return
         pygchem_data = datasets.load(bpch_files)
         # Convert the dataset to an iris cube and export as netCDF
         logging.debug('Creating a netCDF file from bpch.')
-        import iris
-        iris.fileformats.netcdf.save(pygchem_data, netCDF_filename)
-        netCDF_data = Dataset(netCDF_filename)
+        print "Creating a netCDF file from bpch"
+        datasets.save( pygchem_data, netCDF_filename)
+        netCDF_data = datasets.load( netCDF_filename )
+        
         if (netCDF_data == None):
             logging.error('Error creating netCDF file from bpch.')
             raise IOError('Error creating netCDF file from bpch.')
+    
+        # Confirm that the netCDF file time size is the same size 
+        #  as the size of the list
+        # To-do: There is probably a cleaner way to do this.
+        if not (len(netCDF_data.variables['time']) == len(bpch_files)):
+            logging.error('Incorrect amount of timesets from bpch files.'\
+                          'Could be due to incomplete bpch files')
+            # Find any potential small files
+            for bpch_file in bpch_files:
+                if (os.path.getsize( bpch_file ) < 1000):
+                    logging.info('{filename} looks rather small...'\
+                                .format(filename=bpch_file) )
+            raise IOError('Incorrect amount of timesets from bpch files.')
+                        
+                            
+            
+
 
     return netCDF_data
     
